8dcc36c008439e57d1cf9c70710d4a69be72a1e3
Eugene.Samoylov <Eugene.Samylov@eltex.loc>
2013-09-27 09:10:03 +0700

    fix typos and path

--- deodex.sh
+++ deodex.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
+#Uncompiling app/UserDictionaryProvider.odex
+#Adding classes.dex to app/UserDictionaryProvider.jar
+
+
 ERROR=0
-smalibaksmali_dir=/home/mistadman/bin/android_dev_tools
+smalibaksmali_dir=/home/samoylov/.bin
+classpath=":com.android.location.provider.jar:javax.obex.jar:core.jar:apache-xml.jar:bouncycastle.jar"
 
-clear; for x in `find -iname "*.odex"|sort`; do 
-    odexFile=${x/\.\//}
-    [ -e ${file1/odex/jar} ] && JarFile=${odexFile/odex/jar} || Jarfile=${odexFile/odex/apk}
+clear; for x in `find -iname "*.odex"|sort`; do
+odexFile=${x/\.\//}
+     [ -e ${x/odex/jar} ] && Jarfile=${odexFile/odex/jar} || Jarfile=${odexFile/odex/apk}
 
-    echo "Uncompiling $odexFile"
-    java -Xmx512m -jar $smalibaksmali_dir/baksmali.jar -x $odexFile -o /tmp/$odexFile.out 
+    echo "Uncompiling $odexFile for $Jarfile"
+			java -Xmx512m -jar $smalibaksmali_dir/baksmali.jar -x $odexFile -o $odexFile.out -c $classpath -d framework
 
-    if [ -e /tmp/$odexFile.out ]; then
-        java -Xmx512m -jar $smalibaksmali_dir/smali.jar /tmp/$odexFile.out -o /tmp/$odexFile-classes.dex
+    if [ -e $odexFile.out ]; then
+			java -Xmx512m -jar $smalibaksmali_dir/smali.jar $odexFile.out -o $odexFile-classes.dex
         ERROR=1
     fi
 
-    if [ -e /tmp/$odexFile-classes.dex ]; then
-        echo "Adding classes.dex to $JarFile"
-        mv /tmp/$odexFile-classes.dex /tmp/classes.dex
-        zip -q $JarFile /tmp/classes.dex
-        rm -rf /tmp/$odexFile.out /tmp/$odexFile-classes.dex /tmp/classes.dex
+		if [ -e $odexFile-classes.dex ]; then
+			echo "Adding classes.dex to $Jarfile"
+      mv $odexFile-classes.dex classes.dex
+      zip -q $Jarfile classes.dex
+      rm -rf $odexFile.out $odexFile-classes.dex classes.dex $odexFile
     else
-        rm -rf /tmp/$odexFile.out /tmp/$odexFile-classes.dex /tmp/classes.dex
-        ERROR=1
-        echo "Error!"
+			rm -rf $odexFile.out $odexFile-classes.dex classes.dex
+      ERROR=1
+      echo "Error!"
     fi
 
-    echo
+echo
 done
 
-if [ $ERROR -eq 1 ]; then
-    rm -rf *.odex
-else
-    echo "Error(s) detected. *.odex files not deleted."
-fi
